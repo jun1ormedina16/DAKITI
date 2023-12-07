9360423e977b1389d706ab39fa01df47b4da9652
Sr_Cave_0805 <110733424+Cuevitax0805@users.noreply.github.com>
2023-12-07 14:15:04 -0500

    aportes 4to trimestre
    aquí agrego mi aporte al desarrollo de nuestro aplicativo en el 4to trimestre

---
+++ neg_inicio.php
@@ -0,0 +1,40 @@
+<?php
+include('conexion.php'); // Asegúrate de que el archivo de conexión esté correctamente configurado.
+
+$correo = $_POST['correo'];
+$contrasena = $_POST['contrasena'];
+session_start();
+$_SESSION['correo'] = $correo;
+
+// Encripta la contraseña proporcionada por el usuario con MD5
+$contrasena_encriptada = md5($contrasena);
+
+$consulta = "SELECT * FROM usuarios WHERE correo='$correo' AND contrasena='$contrasena_encriptada'";
+$resultado = mysqli_query($db, $consulta);
+
+if (!$resultado) {
+    die('Error en la consulta: ' . mysqli_error($db));
+}
+
+$filas = mysqli_num_rows($resultado);
+
+if ($filas) {
+    $usuario = mysqli_fetch_assoc($resultado);
+    $id_rol = $usuario['id_rol'];
+    
+    if ($id_rol == 1) {
+        // Redirigir al administrador a una página de administrador
+        header("location: indexadmin.php");
+    } elseif ($id_rol == 2) {
+        // Redirigir al usuario normal a una página de usuario
+        header("location: indexuser.php");
+    } 
+} else {
+    // Si la autenticación falla, puedes mostrar un mensaje de error.
+    include("iniciarsesion.html");
+    echo '<h1 class="bad">ERROR DE AUTENTIFICACION</h1>';
+}
+
+mysqli_free_result($resultado);
+mysqli_close($db);
+?>
